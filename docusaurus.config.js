9d79a42af8fcb4a5d3c73c582f96fbbd8db045e1
王朋坤 <44103871+pkcile@users.noreply.github.com>
2024-10-27 17:02:35 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: "https://turfjs.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/turf-www-nuxt/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
